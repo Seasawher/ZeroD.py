36378a216959415bce287cad6932c06c31ec4894
Seasawher <tellagoodstory@icloud.com>
2025-01-06 20:33:07 +0900

    4.5.2 読了 🎉

--- ch04/two_layer_net.py
+++ ch04/two_layer_net.py
@@ -1,15 +1,15 @@
-import sys
-import os
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
-
-sys.path.append(os.pardir)
-from common.functions import sigmoid, softmax, cross_entropy_error
+from common.layers import *
 from common.gradient import numerical_gradient
+from collections import OrderedDict
+
 
 class TwoLayerNet:
-    """2層のニューラルネットワーク"""
 
-    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
         # 重みの初期化
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
@@ -17,41 +17,36 @@ class TwoLayerNet:
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
+        # レイヤの生成
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+        self.lastLayer = SoftmaxWithLoss()
+
     def predict(self, x):
-        """予測値を計算する"""
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        for layer in self.layers.values():
+            x = layer.forward(x)
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
+        return x
 
-        return y
-
+    # x:入力データ, t:教師データ
     def loss(self, x, t):
-        """損失関数を計算する
-
-        ## Parameters:
-        * x(ndarray): 入力データ
-        * t(ndarray): 教師データ
-        """
         y = self.predict(x)
-
-        return cross_entropy_error(y, t)
+        return self.lastLayer.forward(y, t)
 
     def accuracy(self, x, t):
-        """予測の精度（正答率）を求める"""
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
 
+    # x:入力データ, t:教師データ
     def numerical_gradient(self, x, t):
-        """数値微分によって重みパラメータに対する勾配を求める"""
-        loss_W = lambda W: self.loss(x, t)  # noqa: E731
+        loss_W = lambda W: self.loss(x, t)
 
         grads = {}
         grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
@@ -61,3 +56,22 @@ class TwoLayerNet:
 
         return grads
 
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 設定
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
